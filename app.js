882edacfb128c8458bac416b7078d22e46cd48f8
Rahul <rahulkumarmuz1201@gmail.com>
2024-01-01 04:04:11 +0530

    completed cash manager

--- app.js
+++ app.js
@@ -5,27 +5,24 @@ const msg = document.querySelector("#error_msg");
 const no_of_Notes = document.querySelectorAll(".no_of_notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-
 check_button.addEventListener("click", () => {
-  // hideMessage();
+  hideMessage();
   if (Bill_amount.value > 0) {
-    if (cash_given.value >= Bill_amount.value) {
+    if ( parseInt(cash_given.value)>= parseInt (Bill_amount.value)) {
       const amountToBeReturned = cash_given.value - Bill_amount.value;
       calculateChange(amountToBeReturned);
     } else {
-      showMessage(
-        "Do you wanna wash plates?"
-      );
+      showMessage("Do you wanna wash plates?");
     }
   } else {
-    showMessage("envalid Bill Amount");
+    showMessage("invalid Bill Amount");
   }
 });
 
 function calculateChange(amountToBeReturned) {
   for (let i = 0; i < availableNotes.length; i++) {
     const numberofNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-    amountToBeReturned %= availableNotes[i];
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
     no_of_Notes[i].innerText = numberofNotes;
   }
 }
@@ -35,6 +32,7 @@ function hideMessage() {
 }
 
 function showMessage(message) {
-  //   msg.style.display = "block";
+    msg.style.display = "block";
+  console.log(msg);
   msg.innerText = message;
 }
